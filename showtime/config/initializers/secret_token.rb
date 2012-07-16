97a9d933328199f7fea2fcdf7646153da7c85e85
INOUE Takuya <inouetakuya5@gmail.com>
2012-07-16 18:55:42 +0900

    19.2 Rails3 プロジェクトのセットアップ

---
+++ showtime/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtime::Application.config.secret_token = 'e0621241918cbd083fb198122fe463504cddf827e826edc403051444c4d1b07cca29e1479bc4bbef190eab54e4ed153cd5e37e1e72b992a46a27e85a53c2eabb'
